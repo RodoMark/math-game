7f1c750e2c7920e081581c62954bc4f0d34c3eee
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-03-01 20:38:18 +0000

    refactor code from main into Game.rb

--- Game.rb
+++ Game.rb
@@ -1,29 +1,51 @@
 class Game
-  attr_accessor :players, :game_over, :index
+  attr_accessor :players, :game_over, :index, :current_player
 
   def initialize
-    @players = []
+    @player1 = Player.new('Player 1')
+    @player2 = Player.new('Player 2')
+    @current_player = @player1
     @game_over = false
     @index = 0
   end
 
   def check
-    players.each do |p|
-      if p.lives == 0
-        @game_over = true
-      end
+    if @current_player.lives == 0
+      @game_over = true
     end
-
-    def next_turn
-      if @index >= self.players.length - 1 
-        @index = 0
-      else
-        @index += 1
-      end
-      "----- NEW TURN -----"
-    end
-
   end
+
+  def next_turn
+    if @current_player == @player1
+      @current_player = @player2
+    else
+      @current_player = @player1
+    end
+    "----- NEW TURN -----"
+  end
+  
+  def start
+    while game_over == false do
+      prompt = Prompt.new(@current_player.name)
+    
+      is_correct_answer = prompt.question
+    
+      if is_correct_answer
+        puts "That's correct!"
+      else
+        puts "You are wrong."
+        @current_player.lives -= 1
+      end
+    
+      pp "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
+    
+      check
+      next_turn
+    end
+  end
+    
+
+end
 
   
 
@@ -32,8 +54,3 @@ class Game
   # def new_game
 
   # end
-
-
-
-  
-end
--- main.rb
+++ main.rb
@@ -7,35 +7,15 @@ game = Game.new
 
 
 
-player1 = Player.new('Player 1')
-game.players.push(player1)
-player2 = Player.new('Player 2')
-game.players.push(player2)
+
 
 pp game.players
 
  
-while game.game_over == false do
-  current_player = game.players[game.index]
-  prompt = Prompt.new(current_player.name)
-
-  is_correct_answer = prompt.question
-
-  if is_correct_answer
-    puts "That's correct!"
-  else
-    puts "You are wrong."
-    current_player.lives -= 1
-  end
-
-  pp "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
-
-  game.check
-  game.next_turn
-end
+game.start
  
 puts "-----GAME OVER!-----" 
-puts "#{current_player.name} wins."
+puts "#{game.current_player.name} wins."
 
 
 

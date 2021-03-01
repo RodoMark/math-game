8e3f2571176a8b378679c23b931ecfe66c361809
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-03-01 20:02:41 +0000

    add next_turn functionality to Game.rb and main.rb

--- Game.rb
+++ Game.rb
@@ -1,17 +1,28 @@
 class Game
-  attr_accessor :players, :game_over
+  attr_accessor :players, :game_over, :index
 
   def initialize
     @players = []
     @game_over = false
+    @index = 0
   end
 
   def check
     players.each do |p|
       if p.lives == 0
-        self.game_over = true
+        @game_over = true
       end
     end
+
+    def next_turn
+      if @index >= self.players.length - 1 
+        @index = 0
+      else
+        @index += 1
+      end
+    end
+
+
   end
 
   
--- main.rb
+++ main.rb
@@ -3,16 +3,9 @@ require './Game.rb'
 require './Player.rb'
 require './Prompt.rb'
 
-index = 0
-
 game = Game.new
 
-def next_turn
-  index += 1
-  if index > 1 
-    index = 0
-  end
-end
+
 
 player1 = Player.new('Player 1')
 game.players.push(player1)
@@ -22,8 +15,8 @@ game.players.push(player2)
 pp game.players
 
  
-while !game.game_over do
-  current_player = game.players[index]
+while game.game_over == false do
+  current_player = game.players[game.index]
   prompt = Prompt.new(current_player.name)
 
   is_correct_answer = prompt.question
@@ -35,12 +28,10 @@ while !game.game_over do
     current_player.lives -= 1
   end
 
-  pp current_player.lives
-
-  player1.lives = 0
+  pp "#{current_player.name} lives => #{current_player.lives}"
 
   game.check
-  next_turn
+  game.next_turn
 end
   
 
